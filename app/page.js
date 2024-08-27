a0b2d02a9b4f6c587c6fe9669761745ac6d42f8f
Godwins Tuyishime <gtuyishime02@gmail.com>
2024-08-27 00:10:10 -0400

    init commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,141 @@
+'use client';
+
 import Image from "next/image";
-import styles from "./page.module.css";
+import {
+  Box, 
+  Stack, 
+  Typography, 
+  Button,
+  TextField
+} from "@mui/material";
+import { useState, useEffect } from "react";
+import { firestore } from "@/firebase";
+import {
+  collection,
+  doc,
+  setDoc,
+} from 'firebase/firestore';
 
 export default function Home() {
+  const [inventory, setInventory] = useState([])
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [suggestion, setSuggestion] = useState('')
+
+  
+  // Function to handle adding an item to the waitlist
+  const addItemToWaitlist = async () => {
+    try {
+      const docRef = doc(collection(firestore, 'Waitlist'), name); // Document ID is the user's name
+      await setDoc(docRef, {
+        name: name,
+        email: email,
+        suggestion: suggestion,
+      });
+      alert('You have been added to the waitlist!');
+    } catch (error) {
+      console.error("Error adding to waitlist: ", error);
+      alert('There was an error adding you to the waitlist.');
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Box id="background"
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      p={2}
+      width="100vw"
+      height="100vh"
+    >
+      <Stack>
+        <Stack id="landing"
+          width="100vw"
+          sx={{
+            height:{md:"40vh", xs:"20vh"},
+            // paddingTop:{xs:20}
+          }}
+          display="flex"
+          flexDirection="column"
+          justifyContent="center"
+          alignItems="center"
+          spacing={2}
+        >
+          <Typography variant="h1" p={0} sx={{ textAlign:"center", fontSize: {xs:'2rem', md:'5rem', lg:'7rem'}}} >
+            Welcome to the Team!
+          </Typography>
+          <Typography variant="h5" gutterBottom p={1} sx={{ textAlign:"center", fontSize: {xs:'1rem', md:'2rem', lg:'3rem'}}} >
+            See what we're cooking next.
+          </Typography>
+          
+          <Button variant="contained"
+            sx={{
+              fontSize:'1rem',
+              borderRadius: '50px', 
+              bgcolor:"blue", 
+              color:"white",
+              "&:hover": {
+                bgcolor: 'rgba(2, 2, 2, 0.7)',
+                color: 'white',
+              }
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            Get Priority Access
+          </Button>
+          <Box 
+            id="waitlist"
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            alignItems="center"
+            border="1px solid white"
+            p={2}
+          >
+            <Stack spacing ={2}>
+              <TextField 
+                id="name-field" 
+                label="Name" 
+                variant="outlined" 
+                fullWidth
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                sx={{
+                  borderRadius:"10px",
+                  bgcolor:"white"
+                }}
+              />
+              <TextField 
+                id="email-field" 
+                label="Email" 
+                variant="outlined" 
+                fullWidth
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                sx={{
+                  borderRadius:"10px",
+                  bgcolor:"white"
+                }}
+              />
+              <TextField 
+                id="suggestion-field" 
+                label="Suggestions" 
+                variant="outlined" 
+                fullWidth
+                value={suggestion}
+                onChange={(e) => setSuggestion(e.target.value)}
+                sx={{
+                  borderRadius:"10px",
+                  bgcolor:"white"
+                }}
+              />
+              <Button variant="contained" onClick={addItemToWaitlist}>
+                Join
+              </Button>
+            </Stack>
+          </Box>
+        </Stack>
+      </Stack>
+    </Box>
   );
 }
---
+++ firebase.js
@@ -0,0 +1,21 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDqFAxcUq-99_mdGHLiGaoCygjnqo2IOdM",
+  authDomain: "hs-final-4fc8d.firebaseapp.com",
+  projectId: "hs-final-4fc8d",
+  storageBucket: "hs-final-4fc8d.appspot.com",
+  messagingSenderId: "573005251189",
+  appId: "1:573005251189:web:a28dfa0773272f9672ae3c",
+  measurementId: "G-KQXHZS6X6K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore = getFirestore(app);
+
+export {firestore}
