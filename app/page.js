d7adeaef9c750ea860cb1e67f5735c182fe6bceb
Godwins Tuyishime <gtuyishime02@gmail.com>
2024-08-27 01:01:10 -0400

    final

--- app/page.js
+++ app/page.js
@@ -45,9 +45,16 @@ export default function Home() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
-      p={2}
+      p={3}
       width="100vw"
       height="100vh"
+      sx={{
+        backgroundImage:`url('/back.jpg')`,
+        backgroundSize:'cover',
+        backgroundPosition:'center',
+        backgroundRepeat:'repeat-y',
+        backgroundColor:"black"
+      }}
     >
       <Stack>
         <Stack id="landing"
@@ -62,27 +69,13 @@ export default function Home() {
           alignItems="center"
           spacing={2}
         >
-          <Typography variant="h1" p={0} sx={{ textAlign:"center", fontSize: {xs:'2rem', md:'5rem', lg:'7rem'}}} >
+          <Typography variant="h1" p={0} sx={{ textAlign:"center", fontSize: {xs:'1.8rem', md:'5rem', lg:'7rem'}}} >
             Welcome to the Team!
           </Typography>
-          <Typography variant="h5" gutterBottom p={1} sx={{ textAlign:"center", fontSize: {xs:'1rem', md:'2rem', lg:'3rem'}}} >
+          <Typography variant="h5" gutterBottom p={1} sx={{ textAlign:"center", fontSize: {xs:'1.2rem', md:'2rem', lg:'3rem'}}} >
             See what we&apos;re cooking next.
           </Typography>
           
-          <Button variant="contained"
-            sx={{
-              fontSize:'1rem',
-              borderRadius: '50px', 
-              bgcolor:"blue", 
-              color:"white",
-              "&:hover": {
-                bgcolor: 'rgba(2, 2, 2, 0.7)',
-                color: 'white',
-              }
-            }}
-          >
-            Get Priority Access
-          </Button>
           <Box 
             id="waitlist"
             display="flex"
@@ -91,12 +84,36 @@ export default function Home() {
             alignItems="center"
             border="1px solid white"
             p={2}
-            sx={{borderRadius:"10px"}}
+            sx={{
+              width:{lg:"35%", xs:"80%"},
+              borderRadius:"10px", 
+              transition: 'transform 0.3s ease, box-shadow 0.3s ease',  // Smooth transition for movement and shadow
+              "&:hover": {
+                // bgcolor: '#d32f2f',  // Darker red on hover
+                boxShadow: `
+                  0 0 8px rgba(239, 154, 154, 0.5),  /* 50% transparent light red glow */
+                  0 0 16px rgba(244, 67, 54, 0.3),   /* 30% transparent vibrant red glow */
+                  0 0 24px rgba(211, 47, 47, 0.7)    /* 70% transparent darker red glow */ `,
+                transform: 'translate(0px, -5px)'  // Move 5px to the right and 5px down on hover
+              }
+            }}
           >
-            <Stack spacing ={2}>
+            <Stack 
+              display="flex"
+              flexDirection="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing ={2}>
+
+            <Typography variant="h5" sx={{ textAlign:"center", fontSize: {xs:'1rem', md:'2rem', lg:'3rem'}}} >
+            Join the Priority Access List
+            </Typography>
+            <Typography variant="h5" sx={{ textAlign:"center", fontSize: {xs:'0.5rem', md:'1rem', lg:'1rem'}}} >
+            Be the first to experience what&apos;s coming! Enter your details below and share any suggestions or features you&apos;d love to see. Join our waitlist now and stay ahead of the curve!
+            </Typography>
               <TextField 
                 id="name-field" 
-                label="Name" 
+                label="Name*" 
                 variant="outlined" 
                 fullWidth
                 value={name}
@@ -108,7 +125,7 @@ export default function Home() {
               />
               <TextField 
                 id="email-field" 
-                label="Email" 
+                label="Email*" 
                 variant="outlined" 
                 fullWidth
                 value={email}
@@ -130,8 +147,23 @@ export default function Home() {
                   bgcolor:"white"
                 }}
               />
-              <Button variant="contained" onClick={addItemToWaitlist}>
-                Join
+              <Button 
+                variant="contained"
+                sx={{
+                  fontSize:{xs:"10px", lg:'1rem'},
+                  borderRadius: '10px', 
+                  bgcolor: '#e57373', // Reddish background color on hover
+                  color: '#ffffff',   // Brighter shade of the original background color
+                  "&:hover": {
+                    bgcolor: '#d32f2f',  // Darker red on hover
+                    boxShadow: `
+                      0 0 8px #ef9a9a,   /* Light red glow */
+                      0 0 16px #ef9a9a,  /* Light red glow */
+                      0 0 24px #ef9a9a   /* Light red glow */ `
+                  }
+               }}                
+                  onClick={addItemToWaitlist}>
+                Join Now
               </Button>
             </Stack>
           </Box>
